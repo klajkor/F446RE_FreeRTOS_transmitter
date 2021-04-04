73832d6df3a602e251b300ce26783d2a5257b75e
Robert Klajko <robert_klajko@epam.com>
2021-04-04 19:10:49 +0200

    protocolRetVal_enum introduced for buildFrameToSend function

--- Core/Inc/protocol.h
+++ Core/Inc/protocol.h
@@ -11,18 +11,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define FRAME_START_BYTE_1 0x55
-#define FRAME_START_BYTE_2 0x56
-#define FRAME_CMD_BTN_STATUS 0x31
-#define FRAME_CMD_LED_STATUS 0x32
-#define FRAME_CMD_ADC_VALUE 0x33
-#define FRAME_CMD_ADC_RAW 0x34
-#define FRAME_STATUS_OFF 0x30
-#define FRAME_STATUS_ON 0x31
-#define FRAME_EXT1 0
-#define FRAME_EXT2 0
-#define FRAME_STOP_BYTE_1 0x0D
-#define FRAME_STOP_BYTE_2 0x0A
+#define FRAME_START_BYTE_1 		( 0x55 )
+#define FRAME_START_BYTE_2 		( 0x56 )
+#define FRAME_CMD_BTN_STATUS 	( 0x31 )
+#define FRAME_CMD_LED_STATUS 	( 0x32 )
+#define FRAME_CMD_ADC_VALUE 	( 0x33 )
+#define FRAME_CMD_ADC_RAW 		( 0x34 )
+#define FRAME_STATUS_OFF 		( 0x30 )
+#define FRAME_STATUS_ON 		( 0x31 )
+#define FRAME_EXT1 				( 0x00 )
+#define FRAME_EXT2 				( 0x00 )
+#define FRAME_STOP_BYTE_1 		( 0x0D )
+#define FRAME_STOP_BYTE_2 		( 0x0A )
+#define CRC_DEFAULT_VALUE		( 0xFF )
+#define CRC_POLYNOM				( 0x31 )
 
 typedef union {
 	float f;
@@ -38,7 +40,14 @@ typedef struct
 
 typedef uint8_t messageFrame_t[12];
 
-int buildFrameToSend(uint8_t frameCmdID, unionFloatUint8_t frameData, uint8_t *pFrame);
+typedef enum {
+    protocolRetVal_OK = 0,
+    protocolRetVal_NOK,
+    protocolRetVal_End
+} protocolRetVal_enum;
+
+
+protocolRetVal_enum buildFrameToSend(uint8_t frameCmdID, unionFloatUint8_t frameData, uint8_t *pFrame);
 
 
 uint8_t gencrc8(uint8_t *data, uint8_t len);
--- Core/Src/protocol.c
+++ Core/Src/protocol.c
@@ -9,42 +9,48 @@
 #include "protocol.h"
 
 
-int buildFrameToSend(uint8_t frameCmdID, unionFloatUint8_t frameData, uint8_t *pFrame)
+protocolRetVal_enum buildFrameToSend(uint8_t frameCmdID, unionFloatUint8_t frameData, uint8_t *pFrame)
 {
 	uint16_t i;
 	uint8_t crc_byte;
-	if (pFrame == NULL)
-	{
-		return 0;
-	}
-	memset((char *)pFrame, 0, sizeof(pFrame));
+	protocolRetVal_enum retval;
 
-	pFrame[0]=FRAME_START_BYTE_1;
-	pFrame[1]=FRAME_START_BYTE_2;
-	pFrame[2]=frameCmdID;
-	for(i=0;i<4;i++)
+	if ( NULL != pFrame)
 	{
-		pFrame[i+3]=frameData.u8[i];
-	}
-	pFrame[7]=FRAME_EXT1;
-	pFrame[8]=FRAME_EXT2;
-	crc_byte=gencrc8(pFrame, 9);
-	pFrame[9]=crc_byte;
-	pFrame[10]=FRAME_STOP_BYTE_1;
-	pFrame[11]=FRAME_STOP_BYTE_2;
+		memset((char *)pFrame, 0, sizeof(pFrame));
 
-	return 1;
+		pFrame[0]=FRAME_START_BYTE_1;
+		pFrame[1]=FRAME_START_BYTE_2;
+		pFrame[2]=frameCmdID;
+		for(i=0;i<4;i++)
+		{
+			pFrame[i+3]=frameData.u8[i];
+		}
+		pFrame[7]=FRAME_EXT1;
+		pFrame[8]=FRAME_EXT2;
+		crc_byte=gencrc8(pFrame, 9);
+		pFrame[9]=crc_byte;
+		pFrame[10]=FRAME_STOP_BYTE_1;
+		pFrame[11]=FRAME_STOP_BYTE_2;
+		retval = protocolRetVal_OK;
+	}
+	else
+	{
+		retval = protocolRetVal_NOK;
+	}
+
+	return retval;
 }
 
 uint8_t gencrc8(uint8_t *data, uint8_t len)
 {
-    uint8_t crc = 0xff;
+    uint8_t crc = CRC_DEFAULT_VALUE;
     uint8_t i, j;
     for (i = 0; i < len; i++) {
         crc ^= data[i];
         for (j = 0; j < 8; j++) {
             if ((crc & 0x80) != 0)
-                crc = (uint8_t)((crc << 1) ^ 0x31);
+                crc = (uint8_t)((crc << 1) ^ CRC_POLYNOM);
             else
                 crc <<= 1;
         }
