1f0e35d0201c6b77cc80aad3ed4b0437c77534c0
Robert Klajko <robert_klajko@epam.com>
2021-04-12 22:47:34 +0200

    Task priorities has been corrected according to FreeRTOS documentation

--- Core/Inc/app_tasks.h
+++ Core/Inc/app_tasks.h
@@ -24,11 +24,11 @@
 #define STANDARD_TASK_STACK_SIZE 	( ( configSTACK_DEPTH_TYPE ) 128 * 2)
 #define TX_UART_MSG_STACK_SIZE 		( ( configSTACK_DEPTH_TYPE ) 128 * 4)
 
-#define PRIO_ButtonTestSignal 		( ( UBaseType_t ) 35 )  // Lowest priority task
-#define PRIO_LEDswitcher 			( ( UBaseType_t ) 30 )
-#define PRIO_ADCvoltageRead			( ( UBaseType_t ) 25 )
-#define PRIO_TX_UART_msg			( ( UBaseType_t ) 20 )
-#define PRIO_ButtonRead				( ( UBaseType_t ) 15 )  // Highest priority task
+#define PRIO_ButtonTestSignal 		( ( UBaseType_t ) 8 )  // Lowest priority task
+#define PRIO_LEDswitcher 			( ( UBaseType_t ) 16 )
+#define PRIO_ADCvoltageRead			( ( UBaseType_t ) 32 )
+#define PRIO_TX_UART_msg			( ( UBaseType_t ) 40 )
+#define PRIO_ButtonRead				( ( UBaseType_t ) 48 )  // Highest priority task
 #define Signal_Port 				( GPIOC )
 #define Signal_Pin  				( GPIO_PIN_1 ) // CN8 A4 pin
 
